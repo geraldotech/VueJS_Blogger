9d9ef72608447c41aa6f73692841680c6531922a
geraldo.tech <geraldo.filho92@hotmail.com>
2023-06-10 08:43:13 -0300

    :pencil: :tada: Add geraldoX version

---
+++ geraldox.com/src/posts/Speedtest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <a href="http://fisica.ufpr.br/kurumin/" target="_blank"
+          >Index of /kurumin (ufpr.br)</a
+        >
+      </li>
+      <li>
+        <a
+          href="http://download.adobe.com/pub/adobe/photoshop/win/cs2/Photoshop_CS2_tryout.zip"
+          >PhotoshopCS2</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+module.exports = {};
+</script>
+
+<style>
+</style>
---
+++ geraldox.com/src/posts/Vuejs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>How check Vue Version ?</p>
+    <code> console.log(Vue.version);</code>
+    <p><a href="../src/files/vue.global.js" download>VueJS 3.2.41 Dev</a></p>
+  </div>
+</template>
+<script>
+module.exports = {};
+</script>
+<style scoped>
+code {
+  color: green;
+}
+</style>
