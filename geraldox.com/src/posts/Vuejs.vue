1f63a496c31fd4dbb628224e650095159e15dc26
geraldo.tech <geraldo.filho92@hotmail.com>
2023-06-23 09:19:49 -0300

    :pencil: :books: :zap: DOCS

--- geraldox.com/src/posts/Vuejs.vue
+++ geraldox.com/src/posts/Vuejs.vue
@@ -2,7 +2,11 @@
   <div>
     <p>How check Vue Version ?</p>
     <code> console.log(Vue.version);</code>
-    <p><a href="../src/files/vue.global.js" download>VueJS 3.2.41 Dev</a></p>
+    <p>
+      <a href="../src/files/vue.global_3.2.41_dev.zip" download
+        >VueJS 3.2.41 Dev</a
+      >
+    </p>
   </div>
 </template>
 <script>
