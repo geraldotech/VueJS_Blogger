86a2f21e8b1c28adc90696886073f756265cc92e
gmap-nitro <geraldo.filho92@hotmail.com>
2024-03-07 10:24:23 -0300

    fixing fetch by query params

--- geraldox.com-SmartLoader/src/components/blog/Routersearch.vue
+++ geraldox.com-SmartLoader/src/components/blog/Routersearch.vue
@@ -8,7 +8,7 @@
 
     <h2>Fecth post.id <a href="https://geraldox.com/blog/search?postid=2">click</a></h2>
     <strong>this.$route.query.postid</strong>
-    <!--    <p>{{ filteredData }}</p> -->
+     <p>{{ filterbyID }}</p>
     <h2>Multiple queries category and id</h2>
     <p>
       <strong>/blog/search?category=android&id=4</strong>
@@ -19,7 +19,7 @@
 
 <script>
 /* TESTING .. because this is a nested router */
-const database = '../src/db/data.json'
+const API = '../src/db/data.json'
 /*
 
   Vue3 add query:
@@ -35,19 +35,19 @@ module.exports = {
   mounted() {
     /* EXAMPLE 1 */
 
-      axios.get(database).then(
+      axios.get(API).then(
       (response) =>
         //console.log(response.data.blog.posts)
         setTimeout(() => {
           this.allposts = response.data?.blog.posts
           this.state = true
-        }, 1000)
+        }, 2000)
     ).catch(error => {
           console.error('Error fetching data:', error);
         }); 
 
     /* EXAMPLE 2 */
-    //this.fetchData()
+    this.fetchData()
 
     /* CAN CALL DIRECT OU CALL A FN */
   // this.fetchNative()
@@ -58,14 +58,14 @@ module.exports = {
       state: false,
 
       categoryRes: [],
-      filteredData: [],
+      filterbyID: [],
     }
   },
   methods: {
     /* === USE method to fetch and another to filter data  === */
     fetchData() {
       axios
-        .get('/src/db/data.json')
+        .get(API)
         .then((response) => {
           this.rawData = response.data?.blog.posts
           this.handlerFilterfromFN()
@@ -75,14 +75,12 @@ module.exports = {
         })
     },
     handlerFilterfromFN() {
-      // console.log(this.$route.query.postid)
-      this.filteredData = this.rawData.filter((post) => post.id == this.$route.query.postid)
+      this.filterbyID = this.rawData.filter((post) => post.id == this.$route.query.postid)
     },
     async fetchNative() {
       await new Promise((response) => setTimeout(response, 2000))
-
       try {
-        const req = await fetch(database)
+        const req = await fetch(API)
         const data = await req.json()
         console.log(data)
       } catch (err) {
@@ -90,13 +88,12 @@ module.exports = {
       }
     },
   },
+
   /* === USE computed to get posts  === */
   computed: {
     getPostsFromCategory() {
-      // no forget pust ? to void error
-
+      
       if (this.state) {
-        //console.warn(this.allposts)
         //console.log(this.$route.query.category ?? 'no category')
         return this.allposts?.filter((post) => post.category == this.$route.query.category)
       }
