8e0698247994f0a3a90643a298308cb1591f09c7
geraldo.tech.dell <geraldo.filho92@hotmail.com>
2026-01-20 09:59:56 -0300

    var names toLower case

--- geraldox.com-SmartLoader_VueJS3/src/router/index.js
+++ geraldox.com-SmartLoader_VueJS3/src/router/index.js
@@ -1,12 +1,12 @@
-const Home = () => loadModule('/src/views/Home.vue', options)
-const About = () => loadModule('../src/views/About.vue', options)
-const Blog = () => loadModule('../src/components/blog/Blog.vue', options)
-const Mapa = () => loadModule('../src/components/blog/mapa.vue', options)
-const Routersearch = () => loadModule('/src/components/blog/Routersearch.vue', options)
-const AdvancedSearch = () => loadModule('/src/components/blog/AdvancedSearch.vue', options)
-const Categories = () => loadModule('/src/components/blog/Categories.vue', options)
+const home = () => loadModule('/src/views/Home.vue', options)
+const about = () => loadModule('../src/views/About.vue', options)
+const blog = () => loadModule('../src/components/blog/Blog.vue', options)
+const mapa = () => loadModule('../src/components/blog/mapa.vue', options)
+const routersearch = () => loadModule('/src/components/blog/Routersearch.vue', options)
+const advancedSearch = () => loadModule('/src/components/blog/AdvancedSearch.vue', options)
+const categories = () => loadModule('/src/components/blog/Categories.vue', options)
 const singlePost = () => loadModule('/src/components/blog/singlePost.vue', options)
-const NotFound = { template: "<h1 style='color:red'>Not Found</h1>" }
+const notFound = { template: "<h1 style='color:red'>Not Found</h1>" }
 
 // from https://github.com/FranckFreiburger/vue3-sfc-loader/discussions/172
 function load(path) {
@@ -14,36 +14,36 @@ function load(path) {
 }
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: home },
 
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
-  { path: '/about', component: About },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: notFound },
+  { path: '/about', component: about },
   {
     path: '/blog',
     name: 'Blog Posts',
-    component: Blog,
+    component: blog,
     children: [
       {
         path: 'mapa',
         name: 'categoriesMap',
-        component: Mapa,
+        component: mapa,
       },
       {
         path: 'search', // search?name=geraldo
         name: 'search',
-        component: Routersearch,
+        component: routersearch,
       },
       {
         path: 'advancedsearch', // advancedsearch?name=geraldo
         name: 'advancedsearch',
-        component: AdvancedSearch,
+        component: advancedSearch,
       },
       /* DYNAMIC ROUTER :anything below must be in the end */
       /* TO SEND PARAMS IN ROUTER USE path */
       {
         path: ':category',
         name: 'category',
-        component: Categories,
+        component: categories,
       },
       {
         path: ':category/:slug+',
